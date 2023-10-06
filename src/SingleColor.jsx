c0c5e59025a24bec234136e9cefbf919726dee3b
David Castellanos <davcastellanoslarios@gmail.com>
2023-10-06 15:15:10 -0600

    copied color done

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,10 +1,30 @@
+import { toast } from "react-toastify";
 
 export const SingleColor = ({index, color}) => {
 
     const { hex, weight } = color;
 
+    const saveToClipboard = async () =>{
+        if(navigator.clipboard){
+         try {
+
+            await navigator.clipboard.writeText(`#${hex}`)
+            toast.success('Color copied')
+            
+        } catch (error) {
+            toast.error('Failed to copy color')
+         }   
+        }else{
+            toast.error('Clipboard access not available')
+        }
+    }
+
   return (
-    <article className={index > 10 ? 'color color-light' : 'color'} style={{ background:`#${hex}`}}>
+    <article 
+        className={index > 10 ? 'color color-light' : 'color'} 
+        style={{ background:`#${hex}`}}
+        onClick={saveToClipboard}
+    >
         <p className="percent-value">{weight}%</p>
         <p className="color-value">#{hex}</p>
     </article>
